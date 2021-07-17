0ac230f1c9f6b785646acf1eb205bbf407468e54
Tag <tag@protopie.io>
2021-07-17 20:49:06 +0900

    test added

--- test/getNextMino.spec.ts
+++ test/getNextMino.spec.ts
@@ -1,8 +1,11 @@
 import {getNextMino} from '../src/blocks';
 
 describe('getNextMino', () => {
+    let cnt = 0;
+
     it('getNextMino returns a mino', () => {
         const result = getNextMino();
+        cnt++;
 
         expect(result).not.toBeNull();
     });
@@ -12,7 +15,8 @@ describe('getNextMino', () => {
         let diff = false;
 
         for(let i = 0; i < 10; i++) {
-            result = getNextMino();
+            const result = getNextMino();
+            cnt++;
             if (first !== result) {
                 diff = true;
                 break;
@@ -23,6 +27,10 @@ describe('getNextMino', () => {
 
     it('getNextMino returns a mino which is not appears in last 6 times', () => {
         const results = [];
+        while (cnt % 7 !== 6) {
+            getNextMino();
+            cnt++;
+        }
         for(let i = 0; i < 7; i++) {
             results.push(getNextMino());
         }
