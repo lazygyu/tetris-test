86cd729083aeeca06e76f57c9864aaef2168fffb
Tag <tag@protopie.io>
2021-07-17 20:11:56 +0900

    add more test

--- test/eraseFilledLines.spec.ts
+++ test/eraseFilledLines.spec.ts
@@ -2,6 +2,14 @@ import {eraseFilledLines} from '../src/blocks';
 
 describe('Erase filled lines', () => {
     let board: number[][];
+
+    function initBoard() {
+        board = new Array(20);
+        for(let i = 0; i < 20; i++){
+            board[i] = new Array(10).fill(0);
+        }
+    }
+
     beforeEach(() => {
         board = new Array(20);
         for(let i = 0; i < 20; i++){
@@ -17,7 +25,7 @@ describe('Erase filled lines', () => {
     });
 
 
-    it('erase filled lines', () => {
+    it('erase a filled line', () => {
         for(let i = 0; i < 10; i++) {
             board[4][i] = i % 2 === 0 ? 1 : 2;
         }
@@ -31,5 +39,23 @@ describe('Erase filled lines', () => {
         board[3][5] = 0;
         board[4][5] = 3;
         expect(result).toEqual(board);
+    });
+
+    it('erase filled lines', () => {
+        for(let i = 0; i < 10; i++) {
+            board[4][i] = i % 2 === 0 ? 1 : 2;
+            board[5][i] = i % 2 === 0 ? 1 : 2;
+            board[8][i] = i % 2 === 0 ? 1 : 2;
+        }
+        board[3][5] = 3;
+        board[6][3] = 3;
+        board[7][9] = 3;
+        const result = eraseFilledLines(board);
+        initBoard();
+        board[6][5] = 3;
+        board[7][3] = 3;
+        board[8][9] = 3;
+        expect(result).toEqual(board);
+
     });
 });
